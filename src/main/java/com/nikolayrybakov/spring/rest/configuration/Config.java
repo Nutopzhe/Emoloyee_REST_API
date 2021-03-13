d12213cb356d94e3c07e3d0e56b03d1a979aaef4
Nikolay Rybakov <ni_fanta@mail.ru>
2021-03-13 17:28:04 +0300

    added configuration

---
+++ src/main/java/com/nikolayrybakov/spring/rest/configuration/Config.java
@@ -0,0 +1,51 @@
+package com.nikolayrybakov.spring.rest.configuration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.nikolayrybakov.spring.rest")
+@EnableWebMvc
+@EnableTransactionManagement
+public class Config {
+
+    @Bean
+    public BasicDataSource dataSource() {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/my_db?useUnicode=true&amp;serverTimezone=UTC&amp;useSSL=false");
+        basicDataSource.setUsername("bestuser");
+        basicDataSource.setPassword("bestuser");
+        return basicDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        Properties hibernateProperties = new Properties();
+
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.nikolayrybakov.spring.rest.entity");
+        sessionFactory.setHibernateProperties(hibernateProperties);
+        return sessionFactory;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+}
