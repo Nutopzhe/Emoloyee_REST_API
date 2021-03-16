32a7c60fc7de8cc6bbdaa0effe9f7ee8488886d5
Nikolay Rybakov <ni_fanta@mail.ru>
2021-03-16 16:11:00 +0300

    add method getEmployeeById

--- src/main/java/com/nikolayrybakov/spring/rest/controller/Controller.java
+++ src/main/java/com/nikolayrybakov/spring/rest/controller/Controller.java
@@ -4,6 +4,7 @@ import com.nikolayrybakov.spring.rest.entity.Employee;
 import com.nikolayrybakov.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class Controller {
     @GetMapping("/employees")
     public List<Employee> showAllEmployees() {
         return employeeService.getAllEmployee();
+    }
+
+    @GetMapping("employees/{id}")
+    public Employee getEmployeeById(@PathVariable int id) {
+        return employeeService.getEmployeeById(id);
     }
 }
